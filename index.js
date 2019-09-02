f4563397dff26a0fc4bf3fd50d7d305ff57abe67
Siddharth Singh <siddharths067@gmail.com>
2019-09-02 19:42:05 +0530

    Rough Workflow done

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 async function main(){
-    const {Account, Connection} = require(`@solana/web3.js`);
+    const {Account, Connection, BpfLoader} = require(`@solana/web3.js`);
     const newSystemAccountWithAirdrop = require(`./utils/new-system-account-with-airdrop`);
+
+    const fs = require(`fs`);
+    const {execSync} = require(`child_process`);
 
     // Establish Connection
     const connection = new Connection("http://localhost:8899");
 
     // Create a new Public Private Key Pair
-    const account = new Account();
-
     // Request Lamports to the new Accounts
-    const {newAccount, transactionSignature} = await newSystemAccountWithAirdrop(connection, 1000);
+    const {account, transactionSignature} = await newSystemAccountWithAirdrop(connection, 1000);
     console.log(transactionSignature);
 
     // Check if the transaction occurred
@@ -23,8 +24,36 @@ async function main(){
         console.log("Allocation of Lamports has not been completed");
     }
 
+    const accountInformation = await connection.getAccountInfo(account.publicKey);
+    console.log(accountInformation);
 
-    
+    // Running Compilation command
+    console.log(`Compiling Project`)
+    const compilationResult = execSync(`./node_modules/\@solana/web3.js/examples/bpf-rust-noop/do.sh build solana-bpf-rust-noop`);
+    console.log(compilationResult.toString(`utf-8`));
+
+    // Loading BPF Code
+    console.log("Reading Shared Object File");
+    const elf = fs.readFileSync("./node_modules/\@solana/web3.js/examples/bpf-rust-noop/target/bpfel-unknown-unknown/release/solana_bpf_rust_noop.so");
+    //console.log(elf);
+    console.log("SO Reading Completed");
+
+    // Uploading BPF SO
+    var attempts = 10;
+    console.log(`Attempting (max ${attempts}) to upload SO on Chain`);
+    while(attempts--){
+        try{
+            console.log(`Uploading BPF SO`);
+            const programId = await BpfLoader.load(connection, account, elf);
+            console.log(`The Program Id is : ${programId}`);
+            break;
+        }
+        catch (err){
+            attempts--;
+            console.log(`Uploading SO failed. Retrying...... (${attempts} attempts left)`);
+            console.log(`Reason for Failure ${err.message}`);
+        }
+    }
 }
 
 main();
--- utils/new-system-account-with-airdrop.js
+++ utils/new-system-account-with-airdrop.js
@@ -8,7 +8,7 @@ module.exports =  async function newSystemAccountWithAirdrop(
   connection,
   lamports = 1,
 ) {
-  const newAccount = new Account();
-  const transactionSignature = await connection.requestAirdrop(newAccount.publicKey, lamports);
-  return {newAccount, transactionSignature};
+  const account = new Account();
+  const transactionSignature = await connection.requestAirdrop(account.publicKey, lamports);
+  return {account, transactionSignature};
 }
