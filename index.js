4772ee55b0a7bd1cb13bfdbff4ca75c0660b3c12
Siddharth Singh <siddharths067@gmail.com>
2019-09-02 17:03:17 +0530

    Basic Solana Interaction

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+async function main(){
+    const {Account, Connection} = require(`@solana/web3.js`);
+    const newSystemAccountWithAirdrop = require(`./utils/new-system-account-with-airdrop`);
+
+    // Establish Connection
+    const connection = new Connection("http://localhost:8899");
+
+    // Create a new Public Private Key Pair
+    const account = new Account();
+
+    // Request Lamports to the new Accounts
+    const {newAccount, transactionSignature} = await newSystemAccountWithAirdrop(connection, 1000);
+    console.log(transactionSignature);
+
+    // Check if the transaction occurred
+    const isAllocatedLamports = await connection.confirmTransaction(transactionSignature);
+    
+    if(isAllocatedLamports){
+        console.log("Account has been allocated Lamports");
+    }
+    else{
+        console.log("Allocation of Lamports has not been completed");
+    }
+
+    
+    
+}
+
+main();
